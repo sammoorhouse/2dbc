53f742d30d6ce1a99fb4502f4391982ed258a4b5
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2015-10-06 13:57:13 -0400

    console.log

--- popup.js
+++ popup.js
@@ -3,9 +3,6 @@ document.addEventListener('DOMContentLoaded', function(){
 	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 		var uri = img.src = tabs[0].url;
 		var encoded = encodeURI(uri);
-		console.log("encoded:");
-		console.log(encoded);
-		
 		var chartUri = "https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl="
 				+ encoded;
 		img.src = chartUri;
